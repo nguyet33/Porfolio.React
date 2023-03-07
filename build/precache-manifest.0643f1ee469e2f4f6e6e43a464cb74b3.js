7661abc80323a7dfb6dce5087142e5e664036e7c
nguyet33 <tdnguyet33@gmail.com>
2023-03-07 00:37:11 -0800

    CI='' npm run build

---
+++ build/precache-manifest.0643f1ee469e2f4f6e6e43a464cb74b3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29df6171abd3282c1bfbaa20cd412c38",
+    "url": "/nguyet33/Porfolio.React/index.html"
+  },
+  {
+    "revision": "e71d0590dfc9d58b6faf",
+    "url": "/nguyet33/Porfolio.React/static/css/main.ad94a53e.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/nguyet33/Porfolio.React/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/nguyet33/Porfolio.React/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e2823611b643d0af00",
+    "url": "/nguyet33/Porfolio.React/static/js/3.0e543891.chunk.js"
+  },
+  {
+    "revision": "e71d0590dfc9d58b6faf",
+    "url": "/nguyet33/Porfolio.React/static/js/main.bac0bd5a.chunk.js"
+  },
+  {
+    "revision": "fdcd2eb6b9afd674fd42",
+    "url": "/nguyet33/Porfolio.React/static/js/runtime-main.8d898d97.js"
+  },
+  {
+    "revision": "86f1ae46fd954811bc6ba4d975f3d338",
+    "url": "/nguyet33/Porfolio.React/static/media/Lil Lib.86f1ae46.png"
+  },
+  {
+    "revision": "221ddca5654211a5f4eea80234f77822",
+    "url": "/nguyet33/Porfolio.React/static/media/Plan A Trip.221ddca5.png"
+  },
+  {
+    "revision": "5035699ad38fd5313bc95c4891f53581",
+    "url": "/nguyet33/Porfolio.React/static/media/auto-tracker-image.5035699a.png"
+  },
+  {
+    "revision": "d8671f834c3c32005de3dcb406167c84",
+    "url": "/nguyet33/Porfolio.React/static/media/living-real-image.d8671f83.png"
+  },
+  {
+    "revision": "27568392d41d9030f398773f460a3f4a",
+    "url": "/nguyet33/Porfolio.React/static/media/recent-photo.27568392.jpg"
+  }
+]);
